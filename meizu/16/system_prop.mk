03280ebb03045d677fb0534619ab3de5e1d303b8
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-12 04:36:33 +0530

    Add: 16: meizu_16-user 8.1.0 OPM1.171019.026 1537501340 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ meizu/16/system_prop.mk
@@ -0,0 +1,5 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=440 \
+    ro.am.reschedule_service=true \
+    ro.lineage.build.vendor_security_patch=2018-09-01 \
+    ro.sys.fw.use_trim_settings=true \

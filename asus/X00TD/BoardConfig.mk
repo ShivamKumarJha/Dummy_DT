4ac4077ae50f3ebd2c552851b5651ec98cb116f8
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-13 15:55:49 +0530

    Add: X00TD: Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1903.050-20190401:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ asus/X00TD/BoardConfig.mk
@@ -0,0 +1,7 @@
+DEVICE_PATH := device/asus/X00TD
+BOARD_VENDOR := asus
+
+# Security patch level
+VENDOR_SECURITY_PATCH := =2019-03-01
+
+-include vendor/asus/X00TD/BoardConfigVendor.mk
---
+++ asus/X00TD/lineage_X00TD.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from X00TD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00TD
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := lineage_X00TD
+PRODUCT_MODEL := ASUS_X00TD
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00TD
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1016 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1903.050-20190401:user/release-keys

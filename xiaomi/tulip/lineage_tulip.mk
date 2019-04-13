f9f95a2eef3840da532cb20ecaeb9e89f06bf0ed
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-13 15:59:21 +0530

    Update: tulip: xiaomi/tulip/tulip:9/PKQ1.180904.001/9.3.28:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ xiaomi/tulip/lineage_tulip.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from tulip device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := tulip
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_NAME := lineage_tulip
+PRODUCT_MODEL := Redmi Note 6 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := tulip
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="tulip-user 9 PKQ1.180904.001 9.3.28 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := xiaomi/tulip/tulip:9/PKQ1.180904.001/9.3.28:user/release-keys

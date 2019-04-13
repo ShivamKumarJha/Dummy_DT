f7de11bbb02fa8eca1cb12fe424565d6f361c72d
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-13 16:38:40 +0530

    Update: PD1829: vivo/PD1829/PD1829:9/PKQ1.181030.001/compiler04012105:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ vivo/PD1829/lineage_PD1829.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from PD1829 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := vivo
+PRODUCT_DEVICE := PD1829
+PRODUCT_MANUFACTURER := vivo
+PRODUCT_NAME := lineage_PD1829
+PRODUCT_MODEL := vivo
+
+PRODUCT_GMS_CLIENTID_BASE := android-vivo
+TARGET_VENDOR := vivo
+TARGET_VENDOR_PRODUCT_NAME := PD1829
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.181030.001 compiler04012105 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := vivo/PD1829/PD1829:9/PKQ1.181030.001/compiler04012105:user/release-keys

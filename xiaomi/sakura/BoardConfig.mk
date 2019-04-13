ff5a42108bbef2f839f1b26f1eae1d6e291bcbe5
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-13 17:12:19 +0530

    Update: sakura: xiaomi/sakura/sakura:9/PKQ1.180917.001/9.4.11:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ xiaomi/sakura/BoardConfig.mk
@@ -0,0 +1,7 @@
+DEVICE_PATH := device/xiaomi/sakura
+BOARD_VENDOR := xiaomi
+
+# Security patch level
+VENDOR_SECURITY_PATCH := =2019-03-05
+
+-include vendor/xiaomi/sakura/BoardConfigVendor.mk
---
+++ xiaomi/sakura/lineage_sakura.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from sakura device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := sakura
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_NAME := lineage_sakura
+PRODUCT_MODEL := Redmi 6 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := sakura
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sakura-user 9 PKQ1.180917.001 9.4.11 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := xiaomi/sakura/sakura:9/PKQ1.180917.001/9.4.11:user/release-keys

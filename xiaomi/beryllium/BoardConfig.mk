b6d8f763cceb4397019cf157c051dd5b11b58348
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-13 16:52:13 +0530

    Update: beryllium: Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.4.11:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ xiaomi/beryllium/BoardConfig.mk
@@ -0,0 +1,7 @@
+DEVICE_PATH := device/xiaomi/beryllium
+BOARD_VENDOR := xiaomi
+
+# Security patch level
+VENDOR_SECURITY_PATCH := =2019-04-01
+
+-include vendor/xiaomi/beryllium/BoardConfigVendor.mk

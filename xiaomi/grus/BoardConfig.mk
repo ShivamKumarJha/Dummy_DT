b36c90fecf63570c4ec0894c5d9b8f3d4e366573
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-13 16:58:59 +0530

    Update: grus: Xiaomi/grus/grus:9/PKQ1.181121.001/9.4.11:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ xiaomi/grus/BoardConfig.mk
@@ -0,0 +1,7 @@
+DEVICE_PATH := device/xiaomi/grus
+BOARD_VENDOR := xiaomi
+
+# Security patch level
+VENDOR_SECURITY_PATCH := =2019-04-01
+
+-include vendor/xiaomi/grus/BoardConfigVendor.mk
---
+++ xiaomi/grus/lineage_grus.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from grus device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := grus
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_NAME := lineage_grus
+PRODUCT_MODEL := MI 9 SE
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := grus
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="grus-user 9 PKQ1.181121.001 9.4.11 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/grus/grus:9/PKQ1.181121.001/9.4.11:user/release-keys

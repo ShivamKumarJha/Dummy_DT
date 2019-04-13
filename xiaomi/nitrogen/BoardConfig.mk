623dd5501bece223021864febcd95323bf9829b7
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-13 17:01:34 +0530

    Update: nitrogen: Xiaomi/nitrogen/nitrogen:9/PKQ1.181007.001/9.4.11:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ xiaomi/nitrogen/BoardConfig.mk
@@ -0,0 +1,7 @@
+DEVICE_PATH := device/xiaomi/nitrogen
+BOARD_VENDOR := xiaomi
+
+# Security patch level
+VENDOR_SECURITY_PATCH := =2019-04-01
+
+-include vendor/xiaomi/nitrogen/BoardConfigVendor.mk
---
+++ xiaomi/nitrogen/lineage_nitrogen.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from nitrogen device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := nitrogen
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_NAME := lineage_nitrogen
+PRODUCT_MODEL := MI MAX 3
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := nitrogen
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="nitrogen-user 9 PKQ1.181007.001 9.4.11 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/nitrogen/nitrogen:9/PKQ1.181007.001/9.4.11:user/release-keys

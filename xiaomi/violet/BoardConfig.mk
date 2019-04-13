c27de57cc3a9f374eec8dd7a95bd9504cd3162ee
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-13 17:14:40 +0530

    Update: violet: xiaomi/violet/violet:9/PKQ1.181203.001/V10.2.6.0.PFHINXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ xiaomi/violet/BoardConfig.mk
@@ -0,0 +1,7 @@
+DEVICE_PATH := device/xiaomi/violet
+BOARD_VENDOR := xiaomi
+
+# Security patch level
+VENDOR_SECURITY_PATCH := =2019-02-01
+
+-include vendor/xiaomi/violet/BoardConfigVendor.mk
---
+++ xiaomi/violet/lineage_violet.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from violet device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := violet
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_NAME := lineage_violet
+PRODUCT_MODEL := Redmi Note 7 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := violet
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V10.2.6.0.PFHINXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := xiaomi/violet/violet:9/PKQ1.181203.001/V10.2.6.0.PFHINXM:user/release-keys

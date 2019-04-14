dd7ef505ac8843428792a119960a3e229b062cb5
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-14 12:24:19 +0530

    Update: dipper: Xiaomi/dipper/dipper:9/PKQ1.180729.001/9.3.1:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/dipper/BoardConfig.mk
+++ xiaomi/dipper/BoardConfig.mk
@@ -4,4 +4,8 @@ BOARD_VENDOR := xiaomi
 # Security patch level
 VENDOR_SECURITY_PATCH := =2019-02-01
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
 -include vendor/xiaomi/dipper/BoardConfigVendor.mk

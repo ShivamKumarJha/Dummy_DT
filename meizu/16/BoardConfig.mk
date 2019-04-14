889eae8d965050a73e6f774160cf246e8ced2ecb
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-14 11:43:49 +0530

    Update: 16: Meizu/meizu_16/16:8.1.0/OPM1.171019.026/1537501340:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- meizu/16/BoardConfig.mk
+++ meizu/16/BoardConfig.mk
@@ -1,6 +1,8 @@
 DEVICE_PATH := device/meizu/16
 BOARD_VENDOR := meizu
 
-# Security patch level
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 -include vendor/meizu/16/BoardConfigVendor.mk

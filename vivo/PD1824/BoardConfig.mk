944ac7e72a09f947ea6ac5fb52d2089de5969554
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-14 12:11:47 +0530

    Update: PD1824: vivo/PD1824/PD1824:9/PKQ1.181216.001/compiler03081254:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- vivo/PD1824/BoardConfig.mk
+++ vivo/PD1824/BoardConfig.mk
@@ -4,4 +4,8 @@ BOARD_VENDOR := vivo
 # Security patch level
 VENDOR_SECURITY_PATCH := =2019-02-01
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
 -include vendor/vivo/PD1824/BoardConfigVendor.mk

db47fc4cd571c3e8264102e626d402e24a82c7b9
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-14 12:17:26 +0530

    Update: PD1838: vivo/PD1838/PD1838:9/PKQ1.181203.001/compiler03290005:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- vivo/PD1838/BoardConfig.mk
+++ vivo/PD1838/BoardConfig.mk
@@ -4,4 +4,8 @@ BOARD_VENDOR := vivo
 # Security patch level
 VENDOR_SECURITY_PATCH := =2019-03-01
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
 -include vendor/vivo/PD1838/BoardConfigVendor.mk

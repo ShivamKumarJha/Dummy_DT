ebf1d26fa42f24146968fb01d9fc99d1dd98de8a
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-14 12:02:15 +0530

    Update: X8: Meizu/meizu_X8/X8:8.1.0/OPM1.171019.026/1546077651:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- meizu/X8/BoardConfig.mk
+++ meizu/X8/BoardConfig.mk
@@ -1,6 +1,8 @@
 DEVICE_PATH := device/meizu/X8
 BOARD_VENDOR := meizu
 
-# Security patch level
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 -include vendor/meizu/X8/BoardConfigVendor.mk

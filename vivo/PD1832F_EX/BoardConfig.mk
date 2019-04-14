a5b33ef1183383acf0860244180b68e9e514695d
ShivamKumarJha <jha.shivam3@gmail.com>
2019-04-14 12:06:47 +0530

    Update: PD1832F_EX: vivo/1818/1818:9/PKQ1.181203.001/compiler02250201:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- vivo/PD1832F_EX/BoardConfig.mk
+++ vivo/PD1832F_EX/BoardConfig.mk
@@ -4,4 +4,8 @@ BOARD_VENDOR := vivo
 # Security patch level
 VENDOR_SECURITY_PATCH := =2019-01-01
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
 -include vendor/vivo/PD1832F_EX/BoardConfigVendor.mk

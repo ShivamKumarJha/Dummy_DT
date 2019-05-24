fbb6e2690070ceec8471310ea640db799d49d06c
ShivamKumarJha <jha.shivam3@gmail.com>
2019-05-24 20:15:29 +0530

    Update: sakura: xiaomi/sakura/sakura:9/PKQ1.180917.001/9.4.11:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/sakura/BoardConfig.mk
+++ xiaomi/sakura/BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/xiaomi/sakura
 BOARD_VENDOR := xiaomi
 
 # Security patch level
-VENDOR_SECURITY_PATCH := =2019-03-05
+VENDOR_SECURITY_PATCH := 2019-03-05
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml

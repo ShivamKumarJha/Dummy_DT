41355ecb916facfb0090e389275d117be6d7dbfa
ShivamKumarJha <jha.shivam3@gmail.com>
2019-05-24 19:38:21 +0530

    Update: PD1821: vivo/PD1821/PD1821:9/PKQ1.181016.001/compiler01102352:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- vivo/PD1821/BoardConfig.mk
+++ vivo/PD1821/BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/vivo/PD1821
 BOARD_VENDOR := vivo
 
 # Security patch level
-VENDOR_SECURITY_PATCH := =2018-12-01
+VENDOR_SECURITY_PATCH := 2018-12-01
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml

e20868fdc5865679b5bac430c82292dc1372deac
ShivamKumarJha <jha.shivam3@gmail.com>
2019-05-24 20:02:44 +0530

    Update: nitrogen: Xiaomi/nitrogen/nitrogen:9/PKQ1.181007.001/9.4.11:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/nitrogen/BoardConfig.mk
+++ xiaomi/nitrogen/BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/xiaomi/nitrogen
 BOARD_VENDOR := xiaomi
 
 # Security patch level
-VENDOR_SECURITY_PATCH := =2019-04-01
+VENDOR_SECURITY_PATCH := 2019-04-01
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml

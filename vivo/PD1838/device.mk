e759e8bbd9c7c3a9642326985122bb3893cf6138
ShivamKumarJha <jha.shivam3@gmail.com>
2019-05-24 19:48:30 +0530

    Update: PD1838: vivo/PD1838/PD1838:9/PKQ1.181203.001/compiler03290005:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- vivo/PD1838/device.mk
+++ vivo/PD1838/device.mk
@@ -1,7 +1,6 @@
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/a2dp_audio_policy_configuration.xml.1:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml.1 \
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
@@ -17,11 +16,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.PD1832F_EX.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.PD1832F_EX.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.PD1838.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.PD1838.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml.1:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml.1 \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration_generic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_generic.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration_stub.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_stub.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml.1:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml.1 \
     $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt \
     $(LOCAL_PATH)/configs/audio/default_volume_tables.PD1832.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.PD1832.xml \
     $(LOCAL_PATH)/configs/audio/default_volume_tables.PD1832F_EX.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.PD1832F_EX.xml \
@@ -39,14 +36,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_tavil.TD1806.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.TD1806.xml \
     $(LOCAL_PATH)/configs/audio/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/r_submix_audio_policy_configuration.xml.1:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml.1 \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_qrd.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths_wcd9340.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9340.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/stub_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/stub_audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/usb_audio_policy_configuration.xml.1:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml.1 \
 
 # GPS
 PRODUCT_COPY_FILES += \

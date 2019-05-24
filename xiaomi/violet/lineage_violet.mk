78df5441fc537d39d02e76206d0a4dbf5120bd78
ShivamKumarJha <jha.shivam3@gmail.com>
2019-05-24 20:17:52 +0530

    Update: violet: xiaomi/violet/violet:9/PKQ1.181203.001/V10.2.7.0.PFHINXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/violet/lineage_violet.mk
+++ xiaomi/violet/lineage_violet.mk
@@ -18,7 +18,7 @@ PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := violet
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V10.2.10.0.PFHINXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V10.2.7.0.PFHINXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := xiaomi/violet/violet:9/PKQ1.181203.001/V10.2.10.0.PFHINXM:user/release-keys
+BUILD_FINGERPRINT := xiaomi/violet/violet:9/PKQ1.181203.001/V10.2.7.0.PFHINXM:user/release-keys

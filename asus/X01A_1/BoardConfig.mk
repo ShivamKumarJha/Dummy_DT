8917356f5926b53ce2f9fd4728b27acfcbb6ebf9
ShivamKumarJha <jha.shivam3@gmail.com>
2019-06-11 13:26:58 +0530

    Update: X01A_1: asus/WW_X01AD/ASUS_X01A_1:9/WW_Phone-201905221734/16.2018.1905.44-20190522:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- asus/X01A_1/BoardConfig.mk
+++ asus/X01A_1/BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/asus/X01A_1
 BOARD_VENDOR := asus
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-03-05
+VENDOR_SECURITY_PATCH := 2019-05-05
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml

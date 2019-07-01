7758e57511a52af522efbad33eca7dc3e906789e
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-02 02:11:39 +0530

    Update: nitrogen: Xiaomi/nitrogen/nitrogen:9/PKQ1.181007.001/9.6.27:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/nitrogen/lineage_nitrogen.mk
+++ xiaomi/nitrogen/lineage_nitrogen.mk
@@ -18,7 +18,7 @@ PRODUCT_MODEL := MI MAX 3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := nitrogen
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="nitrogen-user 9 PKQ1.181007.001 9.4.11 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="nitrogen-user 9 PKQ1.181007.001 9.6.27 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/nitrogen/nitrogen:9/PKQ1.181007.001/9.4.11:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/nitrogen/nitrogen:9/PKQ1.181007.001/9.6.27:user/release-keys

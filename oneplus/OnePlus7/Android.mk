6ca9bc4dcc9dcee35454c496c00df976cbfe21f0
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-07 14:08:27 +0530

    Add: guacamoleb: OnePlus/OnePlus7/OnePlus7:9/PKQ1.190110.001/1906211340:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- oneplus/OnePlus7/Android.mk
+++
@@ -1,5 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-ifeq ($(TARGET_DEVICE),OnePlus7)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-include $(CLEAR_VARS)
-endif
--- oneplus/OnePlus7/AndroidProducts.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_OnePlus7.mk

48d8b7586299fef1a84a1f9bfffb4fcec8ce13ee
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-10 04:10:09 +0530

    Update: lavender: xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.3.6.0.PFGMIXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/lavender/BoardConfig.mk
+++ xiaomi/lavender/BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/xiaomi/lavender
 BOARD_VENDOR := xiaomi
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-06-05
+VENDOR_SECURITY_PATCH := 2019-05-05
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml

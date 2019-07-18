1c5b7f0f04f74b30c4e4a06cf77a91727877a605
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-18 19:05:06 +0530

    Update: whyred: xiaomi/whyred/whyred:9/PKQ1.180904.001/9.7.11:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/whyred/lineage_whyred.mk
+++ xiaomi/whyred/lineage_whyred.mk
@@ -18,7 +18,7 @@ PRODUCT_MODEL := Redmi Note 5
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="whyred-user 9 PKQ1.180904.001 9.6.27 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="whyred-user 9 PKQ1.180904.001 9.7.11 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:9/PKQ1.180904.001/9.6.27:user/release-keys
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:9/PKQ1.180904.001/9.7.11:user/release-keys

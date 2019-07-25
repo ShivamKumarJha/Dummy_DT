8ed130c6d74323af4e070a820bfdeb8a6f902032
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-25 19:17:22 +0530

    Add: aurora: Sony/aurora/aurora:9/TAMA2-2.0.1-190614-0634/1:user/dev-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ sony/aurora/lineage_aurora.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from aurora device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := sony
+PRODUCT_DEVICE := aurora
+PRODUCT_MANUFACTURER := sony
+PRODUCT_NAME := lineage_aurora
+PRODUCT_MODEL := H8116
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+TARGET_VENDOR := sony
+TARGET_VENDOR_PRODUCT_NAME := aurora
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="aurora-user 9 TAMA2-2.0.1-190614-0634 1 dev-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Sony/aurora/aurora:9/TAMA2-2.0.1-190614-0634/1:user/dev-keys

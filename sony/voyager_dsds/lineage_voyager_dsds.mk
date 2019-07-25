8e5154ca189b44070dcb0211c8ec1ae24a6a9c34
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-26 04:15:53 +0530

    Add: voyager_dsds: Sony/voyager_dsds/voyager_dsds:9/P-NILE-CAF-190516-1703/1:user/dev-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ sony/voyager_dsds/lineage_voyager_dsds.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from voyager_dsds device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := sony
+PRODUCT_DEVICE := voyager_dsds
+PRODUCT_MANUFACTURER := sony
+PRODUCT_NAME := lineage_voyager_dsds
+PRODUCT_MODEL := H4413
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+TARGET_VENDOR := sony
+TARGET_VENDOR_PRODUCT_NAME := voyager_dsds
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="voyager_dsds-user 9 P-NILE-CAF-190516-1703 1 dev-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Sony/voyager_dsds/voyager_dsds:9/P-NILE-CAF-190516-1703/1:user/dev-keys

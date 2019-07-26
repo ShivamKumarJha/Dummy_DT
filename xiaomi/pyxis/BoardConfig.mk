f00ba0abeac2715ab334023f4b11e1ea4c40ef74
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-26 22:41:54 +0530

    Update: pyxis: Xiaomi/pyxis/pyxis:9/PKQ1.181121.001/9.7.25:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/pyxis/BoardConfig.mk
+++ xiaomi/pyxis/BoardConfig.mk
@@ -2,10 +2,11 @@ DEVICE_PATH := device/xiaomi/pyxis
 BOARD_VENDOR := xiaomi
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-05-01
+VENDOR_SECURITY_PATCH := 2019-06-01
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 -include vendor/xiaomi/pyxis/BoardConfigVendor.mk

5abf18e4bc6a19b7b68261d3de2b9ed040481264
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-06 00:31:02 +0530

    Add: marlin: google/marlin/marlin:9/PQ3A.190801.002/5670241:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ google/marlin/lineage_marlin.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from marlin device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := marlin
+PRODUCT_MANUFACTURER := google
+PRODUCT_NAME := lineage_marlin
+PRODUCT_MODEL := Pixel XL
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+TARGET_VENDOR := google
+TARGET_VENDOR_PRODUCT_NAME := marlin
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="marlin-user 9 PQ3A.190801.002 5670241 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/marlin/marlin:9/PQ3A.190801.002/5670241:user/release-keys

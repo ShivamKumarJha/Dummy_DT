8bbe87612f6724747364fc2e188086345cea9579
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-06 00:18:27 +0530

    Update: ysl: xiaomi/ysl/ysl:9/PKQ1.181203.001/V10.3.3.0.PEFMIXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/ysl/lineage_ysl.mk
+++ xiaomi/ysl/lineage_ysl.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -18,7 +17,7 @@ PRODUCT_MODEL := Redmi S2
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := ysl
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="ysl-user 9 PKQ1.181203.001 V10.3.3.0.PEFCNXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="ysl-user 9 PKQ1.181203.001 V10.3.3.0.PEFMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := xiaomi/ysl/ysl:9/PKQ1.181203.001/V10.3.3.0.PEFCNXM:user/release-keys
+BUILD_FINGERPRINT := xiaomi/ysl/ysl:9/PKQ1.181203.001/V10.3.3.0.PEFMIXM:user/release-keys

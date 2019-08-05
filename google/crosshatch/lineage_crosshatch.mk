225d3ca61fee76b9e38455d90802ca2693b3ab0b
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-06 00:28:24 +0530

    Update: crosshatch: google/crosshatch/crosshatch:9/PQ3A.190801.002/5670241:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- google/crosshatch/lineage_crosshatch.mk
+++ google/crosshatch/lineage_crosshatch.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -18,7 +17,7 @@ PRODUCT_MODEL := Pixel 3 XL
 PRODUCT_GMS_CLIENTID_BASE := android-google
 TARGET_VENDOR := google
 TARGET_VENDOR_PRODUCT_NAME := crosshatch
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="crosshatch-user 9 PQ3A.190705.003 5600800 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="crosshatch-user 9 PQ3A.190801.002 5670241 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:9/PQ3A.190705.003/5600800:user/release-keys
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:9/PQ3A.190801.002/5670241:user/release-keys

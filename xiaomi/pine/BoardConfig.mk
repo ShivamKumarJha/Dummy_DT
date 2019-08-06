c541f508f829a2204c32e5b99f5756bf900cd92c
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-06 13:01:42 +0530

    Update: pine: Xiaomi/pine_eea/pine:9/PKQ1.190319.001/V10.2.4.0.PCMEUXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/pine/BoardConfig.mk
+++ xiaomi/pine/BoardConfig.mk
@@ -7,5 +7,6 @@ VENDOR_SECURITY_PATCH := 2019-06-01
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 -include vendor/xiaomi/pine/BoardConfigVendor.mk
--- xiaomi/pine/lineage_pine.mk
+++ xiaomi/pine/lineage_pine.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -18,7 +17,7 @@ PRODUCT_MODEL := Redmi 7A
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := pine
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="pine-user 9 PKQ1.190319.001 V10.2.8.0.PCMCNXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="pine-user 9 PKQ1.190319.001 V10.2.4.0.PCMEUXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/pine/pine:9/PKQ1.190319.001/V10.2.8.0.PCMCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/pine_eea/pine:9/PKQ1.190319.001/V10.2.4.0.PCMEUXM:user/release-keys

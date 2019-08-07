f7b345c3061c440c952b4b1051e505cd62a2b996
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-08 02:06:43 +0530

    Add: mainline: google/crosshatch/crosshatch:10/QPP6.190730.005/5773495:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ google/mainline/lineage_mainline.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from mainline device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := mainline
+PRODUCT_MANUFACTURER := google
+PRODUCT_NAME := lineage_mainline
+PRODUCT_MODEL := mainline
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+TARGET_VENDOR := google
+TARGET_VENDOR_PRODUCT_NAME := mainline
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="crosshatch-user 10 QPP6.190730.005 5773495 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:10/QPP6.190730.005/5773495:user/release-keys

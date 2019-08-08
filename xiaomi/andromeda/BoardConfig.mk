9d132cdb056accc8d39871c32181001fd8059ad5
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-09 00:04:30 +0530

    Update: andromeda: Xiaomi/andromeda/andromeda:9/PKQ1.190321.001/V10.3.17.0.PEMCNXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/andromeda/BoardConfig.mk
+++ xiaomi/andromeda/BoardConfig.mk
@@ -2,10 +2,11 @@ DEVICE_PATH := device/xiaomi/andromeda
 BOARD_VENDOR := xiaomi
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-06-01
+VENDOR_SECURITY_PATCH := 2019-07-01
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 -include vendor/xiaomi/andromeda/BoardConfigVendor.mk
--- xiaomi/andromeda/lineage_andromeda.mk
+++ xiaomi/andromeda/lineage_andromeda.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -18,7 +17,7 @@ PRODUCT_MODEL := Mi MIX 3 5G
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := andromeda
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="andromeda-user 9 PKQ1.190321.001 V10.3.18.0.PEMMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="andromeda-user 9 PKQ1.190321.001 V10.3.17.0.PEMCNXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/andromeda/andromeda:9/PKQ1.190321.001/V10.3.18.0.PEMMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/andromeda/andromeda:9/PKQ1.190321.001/V10.3.17.0.PEMCNXM:user/release-keys

e7db2dc9d614c3c0d5c34a96ec5b966a4c1ccc92
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-09 00:00:07 +0530

    Update: raphael: Xiaomi/raphael/raphael:10/QKQ1.190716.003/9.8.8:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/raphael/lineage_raphael.mk
+++ xiaomi/raphael/lineage_raphael.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -18,7 +17,6 @@ PRODUCT_MODEL := Redmi K20 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := raphael
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="raphael-user 9 PKQ1.181121.001 V10.3.14.0.PFKCNXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/raphael/raphael:9/PKQ1.181121.001/V10.3.14.0.PFKCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/raphael/raphael:10/QKQ1.190716.003/9.8.8:user/release-keys

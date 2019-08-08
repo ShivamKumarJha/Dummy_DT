4a631330f62c5670e594e0f5771526773741cb5c
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-08 23:32:20 +0530

    Add: OnePlus6T: OnePlus/OnePlus6T/OnePlus6T:9/PKQ1.180716.001/1907220050:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ oneplus/OnePlus6T/lineage_OnePlus6T.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from OnePlus6T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := oneplus
+PRODUCT_DEVICE := OnePlus6T
+PRODUCT_MANUFACTURER := oneplus
+PRODUCT_NAME := lineage_OnePlus6T
+PRODUCT_MODEL := OnePlus 6T
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+TARGET_VENDOR := oneplus
+TARGET_VENDOR_PRODUCT_NAME := OnePlus6T
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="OnePlus6T-user 9 PKQ1.180716.001 1907220050 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := OnePlus/OnePlus6T/OnePlus6T:9/PKQ1.180716.001/1907220050:user/release-keys

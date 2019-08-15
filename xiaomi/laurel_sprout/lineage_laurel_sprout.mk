95482e86229c14e9da686ef56c7d9a1a0be42c1c
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-15 18:49:27 +0530

    Update: laurel_sprout: Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.5.0.PFQEUXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/laurel_sprout/lineage_laurel_sprout.mk
+++ xiaomi/laurel_sprout/lineage_laurel_sprout.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Mi A3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := laurel_sprout
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="laurel_sprout-user 9 PKQ1.190416.001 V10.3.3.0.PFQMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="laurel_sprout-user 9 PKQ1.190416.001 V10.3.5.0.PFQEUXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/laurel_sprout/laurel_sprout:9/PKQ1.190416.001/V10.3.3.0.PFQMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.5.0.PFQEUXM:user/release-keys

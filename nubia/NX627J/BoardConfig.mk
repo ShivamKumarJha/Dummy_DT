8c41d211eb36557685e2702614e8035fab8ea518
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-20 00:13:24 +0530

    Add: NX627J: nubia/NX627J/NX627J:9/PKQ1.190616.001/eng.nubia.20190814.210405:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ nubia/NX627J/BoardConfig.mk
@@ -0,0 +1,12 @@
+DEVICE_PATH := device/nubia/NX627J
+BOARD_VENDOR := nubia
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2019-06-05
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/nubia/NX627J/BoardConfigVendor.mk
---
+++ nubia/NX627J/lineage_NX627J.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from NX627J device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := nubia
+PRODUCT_DEVICE := NX627J
+PRODUCT_MANUFACTURER := nubia
+PRODUCT_NAME := lineage_NX627J
+PRODUCT_MODEL := NX627J
+
+PRODUCT_GMS_CLIENTID_BASE := android-nubia
+TARGET_VENDOR := nubia
+TARGET_VENDOR_PRODUCT_NAME := NX627J
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="NX627J-user 9 PKQ1.190616.001 eng.nubia.20190814.210405 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := nubia/NX627J/NX627J:9/PKQ1.190616.001/eng.nubia.20190814.210405:user/release-keys

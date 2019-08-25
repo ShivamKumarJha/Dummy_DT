08bb4388910897c800fe2ccc82db9aac1fc26d75
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-25 12:22:32 +0530

    Add: poplar_dsds: Sony/poplar_dsds/poplar_dsds:9/YOSHINO-2.2.0-190725-0908/1:user/dev-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ sony/poplar_dsds/AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_poplar_dsds.mk
---
+++ sony/poplar_dsds/lineage_poplar_dsds.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from poplar_dsds device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := sony
+PRODUCT_DEVICE := poplar_dsds
+PRODUCT_MANUFACTURER := sony
+PRODUCT_NAME := lineage_poplar_dsds
+PRODUCT_MODEL := Xperia XZ1
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+TARGET_VENDOR := sony
+TARGET_VENDOR_PRODUCT_NAME := poplar_dsds
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="poplar_dsds-user 9 YOSHINO-2.2.0-190725-0908 1 dev-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Sony/poplar_dsds/poplar_dsds:9/YOSHINO-2.2.0-190725-0908/1:user/dev-keys

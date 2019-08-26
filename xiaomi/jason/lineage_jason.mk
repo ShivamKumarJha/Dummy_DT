22bcf5d1c17ce76e1614734450aa66d0417e696d
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-26 20:12:20 +0000

    Update: jason: Xiaomi/jason/jason:9/PKQ1.181007.001/9.8.22:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/jason/lineage_jason.mk
+++ xiaomi/jason/lineage_jason.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Mi Note 3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := jason
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="jason-user 9 PKQ1.181007.001 V10.4.1.0.PCHCNXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="jason-user 9 PKQ1.181007.001 9.8.22 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/jason/jason:9/PKQ1.181007.001/V10.4.1.0.PCHCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/jason/jason:9/PKQ1.181007.001/9.8.22:user/release-keys

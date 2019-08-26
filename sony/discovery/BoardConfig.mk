9020d17370eb7df432d21fa38ac87965f27dd2dc
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-26 21:52:06 +0000

    Update: discovery: Sony/discovery/discovery:9/P-NILE-CAF-190723-0742/1:user/dev-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- sony/discovery/BoardConfig.mk
+++ sony/discovery/BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/sony/discovery
 BOARD_VENDOR := sony
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-05-01
+VENDOR_SECURITY_PATCH := 2019-08-01
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
--- sony/discovery/lineage_discovery.mk
+++ sony/discovery/lineage_discovery.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -13,12 +12,12 @@ PRODUCT_BRAND := sony
 PRODUCT_DEVICE := discovery
 PRODUCT_MANUFACTURER := sony
 PRODUCT_NAME := lineage_discovery
-PRODUCT_MODEL := H3213
+PRODUCT_MODEL := Xperia XA2 Ultra
 
 PRODUCT_GMS_CLIENTID_BASE := android-sony
 TARGET_VENDOR := sony
 TARGET_VENDOR_PRODUCT_NAME := discovery
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="discovery-user 9 P-NILE-CAF-190516-1703 1 dev-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="discovery-user 9 P-NILE-CAF-190723-0742 1 dev-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Sony/discovery/discovery:9/P-NILE-CAF-190516-1703/1:user/dev-keys
+BUILD_FINGERPRINT := Sony/discovery/discovery:9/P-NILE-CAF-190723-0742/1:user/dev-keys

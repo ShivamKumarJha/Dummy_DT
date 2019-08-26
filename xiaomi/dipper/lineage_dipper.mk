a6b074d9191cedb7e0de18b2bddac764ed05c92c
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-26 19:32:17 +0000

    Update: dipper: Xiaomi/dipper/dipper:9/PKQ1.180729.001/9.8.22:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/dipper/lineage_dipper.mk
+++ xiaomi/dipper/lineage_dipper.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -18,7 +17,7 @@ PRODUCT_MODEL := MI 8
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := dipper
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="dipper-user 9 PKQ1.180729.001 9.3.1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="dipper-user 9 PKQ1.180729.001 9.8.22 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/dipper/dipper:9/PKQ1.180729.001/9.3.1:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/dipper/dipper:9/PKQ1.180729.001/9.8.22:user/release-keys

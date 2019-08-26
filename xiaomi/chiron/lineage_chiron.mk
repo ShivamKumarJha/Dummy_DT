2ec59abe27354692b4d989a810cf9c8d698b7c93
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-26 20:10:19 +0000

    Update: chiron: Xiaomi/chiron/chiron:9/PKQ1.190118.001/9.8.22:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/chiron/lineage_chiron.mk
+++ xiaomi/chiron/lineage_chiron.mk
@@ -12,12 +12,12 @@ PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := chiron
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_NAME := lineage_chiron
-PRODUCT_MODEL := Mi MIX 2
+PRODUCT_MODEL := MIX 2
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := chiron
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="chiron-user 9 PKQ1.190118.001 V10.4.1.0.PDEMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="chiron-user 9 PKQ1.190118.001 9.8.22 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/chiron/chiron:9/PKQ1.190118.001/V10.4.1.0.PDEMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/chiron/chiron:9/PKQ1.190118.001/9.8.22:user/release-keys

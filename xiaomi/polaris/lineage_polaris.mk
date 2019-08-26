fcf1087e75f8f76e1c978279fb761055a3b7eb47
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-26 19:33:30 +0000

    Update: polaris: Xiaomi/polaris/polaris:9/PKQ1.180729.001/9.8.22:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/polaris/lineage_polaris.mk
+++ xiaomi/polaris/lineage_polaris.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := MIX 2S
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := polaris
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="polaris-user 9 PKQ1.180729.001 9.8.1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="polaris-user 9 PKQ1.180729.001 9.8.22 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/polaris/polaris:9/PKQ1.180729.001/9.8.1:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/polaris/polaris:9/PKQ1.180729.001/9.8.22:user/release-keys

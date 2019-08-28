66eb8ba9e5f7435572b46c3224c5da698fff0d1a
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-28 03:33:41 +0000

    Add: M1822: Meizu/meizu_note8_EEA/meizunote8:8.1.0/OPM1.171019.026/1563415055:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ meizu/M1822/BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/meizu/M1822
+BOARD_VENDOR := meizu
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/meizu/M1822/BoardConfigVendor.mk

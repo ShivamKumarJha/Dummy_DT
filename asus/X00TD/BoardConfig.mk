b0a3d846dbb0b5d6c7d015d0c571fca33aad6049
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-28 02:20:54 +0000

    Update: X00TD: Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1906.057-20190816:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- asus/X00TD/BoardConfig.mk
+++ asus/X00TD/BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/asus/X00TD
 BOARD_VENDOR := asus
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-06-05
+VENDOR_SECURITY_PATCH := 2019-08-05
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml

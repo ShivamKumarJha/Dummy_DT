37cec3f8289a7ff4dd580c1b912c5a1b47a1522a
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-28 09:22:49 +0000

    Update: ZS620KL: asus/ZS620KL:10/52:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- asus/ZS620KL/lineage_ZS620KL.mk
+++ asus/ZS620KL/lineage_ZS620KL.mk
@@ -17,7 +17,6 @@ PRODUCT_MODEL := ASUS_ZS620KL
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := ZS620KL
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC=""
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := asus/ZS620KL:10/52:user/release-keys

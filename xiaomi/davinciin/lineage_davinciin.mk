74ebe51cdde0eab494ba4f7ec51fea812a6ef027
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-31 11:32:58 +0000

    Update: davinciin: Xiaomi/davinciin/davinciin:9/PKQ1.190302.001/V10.3.8.0.PFJINXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/davinciin/lineage_davinciin.mk
+++ xiaomi/davinciin/lineage_davinciin.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Redmi K20
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := davinciin
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="davinciin-user 9 PKQ1.190302.001 V10.3.6.0.PFJINXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="davinciin-user 9 PKQ1.190302.001 V10.3.8.0.PFJINXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/davinciin/davinciin:9/PKQ1.190302.001/V10.3.6.0.PFJINXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/davinciin/davinciin:9/PKQ1.190302.001/V10.3.8.0.PFJINXM:user/release-keys

6bf9d56d2ef481372671e41410cdedd19526a08c
ShivamKumarJha <jha.shivam3@gmail.com>
2019-09-03 17:55:37 +0000

    Update: taimen: google/taimen/taimen:10/QP1A.190711.020/5800535:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- google/taimen/lineage_taimen.mk
+++ google/taimen/lineage_taimen.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Pixel 2 XL
 PRODUCT_GMS_CLIENTID_BASE := android-google
 TARGET_VENDOR := google
 TARGET_VENDOR_PRODUCT_NAME := taimen
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="taimen-user 10 QPP6.190730.005 5773495 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="taimen-user 10 QP1A.190711.020 5800535 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/taimen/taimen:10/QPP6.190730.005/5773495:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:10/QP1A.190711.020/5800535:user/release-keys

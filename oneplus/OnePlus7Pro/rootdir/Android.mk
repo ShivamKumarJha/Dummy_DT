473a6b7670555a007081a621a39de47f2b7a1c83
ShivamKumarJha <jha.shivam3@gmail.com>
2019-09-04 09:06:08 +0000

    Update: OnePlus7Pro: OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/1909010630:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- oneplus/OnePlus7Pro/rootdir/Android.mk
+++ oneplus/OnePlus7Pro/rootdir/Android.mk
@@ -274,6 +274,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab_nodata.qcom
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab_nodata.qcom
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional eng

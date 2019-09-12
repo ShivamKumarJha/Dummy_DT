57af2d52704b20722e39afde48479c991f8e1a9a
ShivamKumarJha <jha.shivam3@gmail.com>
2019-09-12 18:15:58 +0000

    Update: 1804: vivo/1804/1804:9/PKQ1.180819.001/compiler07042010:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- vivo/1804/lineage_1804.mk
+++ vivo/1804/lineage_1804.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -18,7 +17,7 @@ PRODUCT_MODEL := vivo 1804
 PRODUCT_GMS_CLIENTID_BASE := android-vivo
 TARGET_VENDOR := vivo
 TARGET_VENDOR_PRODUCT_NAME := 1804
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1.180819.001 compiler04192130 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1.180819.001 compiler07042010 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := vivo/1804/1804:9/PKQ1.180819.001/compiler04192130:user/release-keys
+BUILD_FINGERPRINT := vivo/1804/1804:9/PKQ1.180819.001/compiler07042010:user/release-keys

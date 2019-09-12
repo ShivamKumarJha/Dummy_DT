0c6fec06cf0798abc3ffd311dd6325155a4c30b4
ShivamKumarJha <jha.shivam3@gmail.com>
2019-09-12 19:00:44 +0000

    Update: 1818: vivo/1818/1818:9/PKQ1.181203.001/compiler07060349:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- vivo/1818/BoardConfig.mk
+++ vivo/1818/BoardConfig.mk
@@ -2,10 +2,11 @@ DEVICE_PATH := device/vivo/1818
 BOARD_VENDOR := vivo
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-03-01
+VENDOR_SECURITY_PATCH := 2019-06-01
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 -include vendor/vivo/1818/BoardConfigVendor.mk
--- vivo/1818/lineage_1818.mk
+++ vivo/1818/lineage_1818.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -18,7 +17,7 @@ PRODUCT_MODEL := vivo 1818
 PRODUCT_GMS_CLIENTID_BASE := android-vivo
 TARGET_VENDOR := vivo
 TARGET_VENDOR_PRODUCT_NAME := 1818
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sm6150-user 9 PKQ1.181203.001 compiler05071942 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sm6150-user 9 PKQ1.181203.001 compiler07060408 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := vivo/1818/1818:9/PKQ1.181203.001/compiler05071925:user/release-keys
+BUILD_FINGERPRINT := vivo/1818/1818:9/PKQ1.181203.001/compiler07060349:user/release-keys

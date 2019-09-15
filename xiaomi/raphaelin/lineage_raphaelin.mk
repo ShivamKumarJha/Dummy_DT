3756348bbef73b8520641ee3401eafd0b695eb2e
ShivamKumarJha <jha.shivam3@gmail.com>
2019-09-15 06:58:53 +0000

    Update: raphaelin: Xiaomi/raphaelin/raphaelin:10/QKQ1.190716.003/V10.4.7.0.QFKINXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/raphaelin/lineage_raphaelin.mk
+++ xiaomi/raphaelin/lineage_raphaelin.mk
@@ -17,6 +17,7 @@ PRODUCT_MODEL := Redmi K20 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := raphaelin
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="raphaelin-user 10 QKQ1.190716.003 V10.4.7.0.QFKINXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/raphaelin/raphaelin:10/QKQ1.190716.003/V10.4.7.0.QFKINXM:user/release-keys

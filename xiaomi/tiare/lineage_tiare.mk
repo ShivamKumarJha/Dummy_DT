451a1c247e8f5246cd3df8b85bde498238aee803
ShivamKumarJha <jha.shivam3@gmail.com>
2019-09-20 20:13:39 +0000

    Update: tiare: Xiaomi/tiare/tiare:8.1.0/OPM1.171019.026/V10.2.13.0.OCLMIXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/tiare/lineage_tiare.mk
+++ xiaomi/tiare/lineage_tiare.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Redmi Go
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tiare
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="tiare-user 8.1.0 OPM1.171019.026 V10.2.6.0.OCLMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="tiare-user 8.1.0 OPM1.171019.026 V10.2.13.0.OCLMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/tiare/tiare:8.1.0/OPM1.171019.026/V10.2.6.0.OCLMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/tiare/tiare:8.1.0/OPM1.171019.026/V10.2.13.0.OCLMIXM:user/release-keys

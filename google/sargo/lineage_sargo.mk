13b69b33362e996d3e654d42f403cd5651ceeed1
ShivamKumarJha <jha.shivam3@gmail.com>
2019-09-24 02:05:48 +0000

    Update: sargo: google/sargo/sargo:10/QP1A.190711.020.C3/5869620:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- google/sargo/lineage_sargo.mk
+++ google/sargo/lineage_sargo.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := mainline
 PRODUCT_GMS_CLIENTID_BASE := android-google
 TARGET_VENDOR := google
 TARGET_VENDOR_PRODUCT_NAME := sargo
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sargo-user 10 QP1A.190711.020 5800535 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sargo-user 10 QP1A.190711.020.C3 5869620 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/sargo/sargo:10/QP1A.190711.020/5800535:user/release-keys
+BUILD_FINGERPRINT := google/sargo/sargo:10/QP1A.190711.020.C3/5869620:user/release-keys

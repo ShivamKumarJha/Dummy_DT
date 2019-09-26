5063cc3eebec9d239aed56dd33387f6bb457d9d3
ShivamKumarJha <jha.shivam3@gmail.com>
2019-09-26 14:00:55 +0530

    Update: lavender: xiaomi/lavender/lavender:9/PKQ1.180904.001/9.9.24:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/lavender/lineage_lavender.mk
+++ xiaomi/lavender/lineage_lavender.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Redmi Note 7
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := lavender
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 9.9.3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 9.9.24 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := xiaomi/lavender/lavender:9/PKQ1.180904.001/9.9.3:user/release-keys
+BUILD_FINGERPRINT := xiaomi/lavender/lavender:9/PKQ1.180904.001/9.9.24:user/release-keys

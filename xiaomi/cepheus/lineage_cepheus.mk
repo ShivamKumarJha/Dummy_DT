ccda0c339bd8b75704c94b3975272317d7b58b21
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-12 10:01:39 +0530

    Update: cepheus: Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/9.10.8:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/cepheus/lineage_cepheus.mk
+++ xiaomi/cepheus/lineage_cepheus.mk
@@ -17,6 +17,7 @@ PRODUCT_MODEL := MI 9
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := cepheus
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="cepheus-user 10 QKQ1.190825.002 9.10.8 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/cepheus/cepheus:10/QKQ1.190716.003/9.8.21:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/9.10.8:user/release-keys
--- xiaomi/cepheus/rootdir/bin/init.qcom.usb.sh
+++ xiaomi/cepheus/rootdir/bin/init.qcom.usb.sh
@@ -180,6 +180,7 @@ if [ -d /config/usb_gadget ]; then
 	fi
 
 	setprop sys.usb.configfs 1
+	setprop vendor.usb.configfs 1
 fi
 
 #

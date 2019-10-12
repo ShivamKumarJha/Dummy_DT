a0889cf32f8f185f171cadb71dce0185fd1f0ad6
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-12 09:54:58 +0530

    Update: mata: essential/mata/mata:10/QP1A.190711.148/446:user/test-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- essential/mata/BoardConfig.mk
+++ essential/mata/BoardConfig.mk
@@ -2,11 +2,12 @@ DEVICE_PATH := device/essential/mata
 BOARD_VENDOR := essential
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-09-05
+VENDOR_SECURITY_PATCH := 2019-10-05
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.1-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.power@1.3-service.mata.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
--- essential/mata/lineage_mata.mk
+++ essential/mata/lineage_mata.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := PH-1
 PRODUCT_GMS_CLIENTID_BASE := android-essential
 TARGET_VENDOR := essential
 TARGET_VENDOR_PRODUCT_NAME := mata
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="mata-user 10 QP1A.190711.122 420 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="mata-user 10 QP1A.190711.148 446 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := essential/mata/mata:10/QP1A.190711.122/420:user/test-keys
+BUILD_FINGERPRINT := essential/mata/mata:10/QP1A.190711.148/446:user/test-keys

959c0631f27865cc886d965fe292aa712f469a53
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-13 15:26:41 +0530

    Add: 1909: vivo/1909/1909:9/PKQ1.190302.001/compiler09070559:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ vivo/1909/lineage_1909.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from 1909 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := vivo
+PRODUCT_DEVICE := 1909
+PRODUCT_MANUFACTURER := vivo
+PRODUCT_NAME := lineage_1909
+PRODUCT_MODEL := vivo 1909
+
+PRODUCT_GMS_CLIENTID_BASE := android-vivo
+TARGET_VENDOR := vivo
+TARGET_VENDOR_PRODUCT_NAME := 1909
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sm6150-user 9 PKQ1.190302.001 compiler09070559 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := vivo/1909/1909:9/PKQ1.190302.001/compiler09070559:user/release-keys

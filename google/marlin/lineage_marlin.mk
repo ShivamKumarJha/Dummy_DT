73c439e8c3f486a39d347f19af1bc69a303e436e
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-13 21:23:58 +0530

    Update: marlin: google/marlin/marlin:10/QP1A.191005.007.A1/5908163:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- google/marlin/lineage_marlin.mk
+++ google/marlin/lineage_marlin.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Pixel XL
 PRODUCT_GMS_CLIENTID_BASE := android-google
 TARGET_VENDOR := google
 TARGET_VENDOR_PRODUCT_NAME := marlin
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="marlin-user 10 QP1A.190711.020 5800535 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="marlin-user 10 QP1A.191005.007.A1 5908163 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/marlin/marlin:10/QP1A.190711.020/5800535:user/release-keys
+BUILD_FINGERPRINT := google/marlin/marlin:10/QP1A.191005.007.A1/5908163:user/release-keys

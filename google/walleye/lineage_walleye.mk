1222c58db4fffa9ee78d8c4e59ff4cdd2f967d0e
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-13 21:17:50 +0530

    Update: walleye: google/walleye/walleye:10/QP1A.191005.007.A1/5908163:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- google/walleye/lineage_walleye.mk
+++ google/walleye/lineage_walleye.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Pixel 2
 PRODUCT_GMS_CLIENTID_BASE := android-google
 TARGET_VENDOR := google
 TARGET_VENDOR_PRODUCT_NAME := walleye
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="walleye-user 10 QP1A.190711.020 5800535 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="walleye-user 10 QP1A.191005.007.A1 5908163 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/walleye/walleye:10/QP1A.190711.020/5800535:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:10/QP1A.191005.007.A1/5908163:user/release-keys

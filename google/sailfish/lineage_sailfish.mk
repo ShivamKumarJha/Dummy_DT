60915010c1aaa610151b5553d5bf4b9d0945a258
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-13 21:30:18 +0530

    Update: sailfish: google/sailfish/sailfish:10/QP1A.191005.007.A1/5908163:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- google/sailfish/lineage_sailfish.mk
+++ google/sailfish/lineage_sailfish.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Pixel
 PRODUCT_GMS_CLIENTID_BASE := android-google
 TARGET_VENDOR := google
 TARGET_VENDOR_PRODUCT_NAME := sailfish
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.190711.020 5800535 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.191005.007.A1 5908163 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.190711.020/5800535:user/release-keys
+BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.191005.007.A1/5908163:user/release-keys

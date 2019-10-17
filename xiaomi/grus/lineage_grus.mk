ee31f6f676005b06142a33d730827e3f9223f7ac
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-17 20:20:21 +0000

    Update: grus: Xiaomi/grus/grus:9/PKQ1.181121.001/V11.0.3.0.PFBCNXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/grus/lineage_grus.mk
+++ xiaomi/grus/lineage_grus.mk
@@ -12,12 +12,12 @@ PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := grus
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_NAME := lineage_grus
-PRODUCT_MODEL := Mi 9 SE
+PRODUCT_MODEL := MI 9 SE
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := grus
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="grus-user 9 PKQ1.181121.001 V10.3.9.0.PFBEUXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="grus-user 9 PKQ1.181121.001 V11.0.3.0.PFBCNXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/grus_eea/grus:9/PKQ1.181121.001/V10.3.9.0.PFBEUXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/grus/grus:9/PKQ1.181121.001/V11.0.3.0.PFBCNXM:user/release-keys

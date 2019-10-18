45a53132fb6b2c6ba2898b424752a7e0b5aaa6c3
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-18 20:07:48 +0000

    Update: equuleus: Xiaomi/equuleus/equuleus:9/PKQ1.180729.001/V11.0.2.0.PECCNXM:user/release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- xiaomi/equuleus/lineage_equuleus.mk
+++ xiaomi/equuleus/lineage_equuleus.mk
@@ -12,12 +12,12 @@ PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := equuleus
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_NAME := lineage_equuleus
-PRODUCT_MODEL := MI 8 Pro
+PRODUCT_MODEL := MI 8 UD
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := equuleus
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="equuleus-user 9 PKQ1.180729.001 V10.3.5.0.PECMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="equuleus-user 9 PKQ1.180729.001 V11.0.2.0.PECCNXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/equuleus/equuleus:9/PKQ1.180729.001/V10.3.5.0.PECMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/equuleus/equuleus:9/PKQ1.180729.001/V11.0.2.0.PECCNXM:user/release-keys
